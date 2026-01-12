8ecb7ffbc4b7ceb6453f1c13caf7a49be7c03f51
Jerry <jeremiahjacob261@gmail.com>
2026-01-12 04:43:34 +0100

    made public

--- src/components/video-section.tsx
+++ src/components/video-section.tsx
@@ -15,13 +15,16 @@ export function VideoSection() {
                 <div className="max-w-5xl mx-auto">
                     <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
                         <iframe
-                            src="https://player.vimeo.com/video/1153456052?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                            src="https://player.vimeo.com/video/1153456052?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+
                             className="absolute top-0 left-0 w-full h-full rounded-2xl shadow-2xl"
                             frameBorder="0"
                             allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
                             referrerPolicy="strict-origin-when-cross-origin"
                             title="HOW TO SWAP IN SPARKEXCHANGE.IO"
                         />
+
+
                     </div>
                 </div>
             </div>
